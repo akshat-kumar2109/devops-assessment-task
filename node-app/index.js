0a43e929b56126e67864bcddba7c0233436cbc14
Akshat Kumar <akshatkumar1101@gmail.com>
2025-04-07 16:28:54 +0530

    Remove redundant code

--- node-app/index.js
+++ node-app/index.js
@@ -3,10 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  logger.info('Handling request to root endpoint');
   res.send('Hello, Paper.Social');
 });
 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
